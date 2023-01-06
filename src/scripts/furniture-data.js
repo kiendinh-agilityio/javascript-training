fd5a4ec544dcf6d2c6c0836f1cf180c11caf5514
Kien Dinh <kien.dinh@asnet.com.vn>
2023-01-06 17:28:42 +0700

    Feature/implement favorites page

--- src/scripts/furniture-data.js
+++ src/scripts/furniture-data.js
@@ -1,3 +1,5 @@
+import { PRODUCT_LOCAL_STORAGE_KEY } from './constant.js';
+
 const defaultFurniture = [
   {
     id: '9bd88c49-d7e0-403e-ab55-6c329ae8d77e',
@@ -25,4 +27,51 @@ const defaultFurniture = [
   }
 ];
 
-export { defaultFurniture }
+const defaultProduct = [
+  {
+    id: '7daccba2-0515-42dd-96b9-4eb1e7c34172',
+    image: 'https://i.imgur.com/kso5QKH.png',
+    name: 'Mini Blue Chair',
+    description: 'Find a bright ideal to suit your taste with our great selection of suspension, wall, floor and table lights. breathable Walking',
+    length: 'Length-34cm',
+    width: 'Width-56cm',
+    price: 'N65,000'
+  },
+  {
+    id: 'e7fbbdb6-4610-4d51-9fae-a5cf58fa5d5d',
+    image: 'https://i.imgur.com/qInEhkj.png',
+    name: 'Mini Blue Chair',
+    description: 'Find a bright ideal to suit your taste with our great selection of suspension, wall, floor and table lights. breathable Walking',
+    length: 'Length-34cm',
+    width: 'Width-56cm',
+    price: 'N65,000'
+  },
+  {
+    id: '5e9f37f5-bcf2-4e77-81a6-bd2acc42e4cf',
+    image: 'https://i.imgur.com/EJcRUYi.png',
+    name: 'Mini Blue Chair',
+    description: 'Find a bright ideal to suit your taste with our great selection of suspension, wall, floor and table lights. breathable Walking',
+    length: 'Length-34cm',
+    width: 'Width-56cm',
+    price: 'N65,000'
+  },
+  {
+    id: '14f9ecc2-fab7-4934-94ba-3fc3b49570fd',
+    image: 'https://i.imgur.com/OdIgCkL.png',
+    name: 'Mini Blue Chair',
+    description: 'Find a bright ideal to suit your taste with our great selection of suspension, wall, floor and table lights. breathable Walking',
+    length: 'Length-34cm',
+    width: 'Width-56cm',
+    price: 'N65,000'
+  }
+];
+
+const data = localStorage.getItem(PRODUCT_LOCAL_STORAGE_KEY);
+if (!data) {
+  localStorage.setItem(PRODUCT_LOCAL_STORAGE_KEY, JSON.stringify(defaultProduct));
+}
+
+export {
+  defaultFurniture,
+  defaultProduct
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,11 @@ import { createElement } from './template';
 import {
   getFavoriteFurnitureFromLocalStorage,
   setFavoriteFurnitureToLocalStorage,
+  changeIcon
 } from './helper.js';
 import { handleAddEventListenerNavMenu } from './navbar.js';
 
-const btnFavoriteList = document.querySelector('.btn-list-favorites');
 const domFurniture = document.querySelector('.furniture-list');
-const favoritesList = document.querySelector('.favorites-list');
-const modalClose = document.querySelector('.btn-close-modal');
 let favoriteItems = getFavoriteFurnitureFromLocalStorage();
 
 const renderListFurnitures = (items) => {
@@ -19,7 +17,7 @@ const renderListFurnitures = (items) => {
   });
 };
 
-const loadData = () => {
+const renderFurniture = () => {
   //format data local vs default
   const formatData = defaultFurniture.map(item => {
     const findFavorite = favoriteItems.indexOf(item.id);
@@ -34,50 +32,6 @@ const loadData = () => {
     };
   });
   renderListFurnitures(formatData);
-};
-
-const filterFavoriteItems = (favoriteItemsTemp) => {
-  return defaultFurniture
-    .reduce((favoriteValue, currentValue) => {
-      const findFavorite = favoriteItemsTemp.indexOf(currentValue.id);
-      if (findFavorite >= 0) favoriteValue.push(currentValue);
-      return favoriteValue;
-    }, [])
-    .map(item => {
-      return {
-        ...item,
-        isFavorite: true
-      };
-    });
-};
-
-// Favorites List
-const handleAddEventListenerForFavoriteButton = () => {
-  btnFavoriteList.addEventListener('click', () => {
-    const showModal = document.getElementById('favorite-modal');
-    showModal.classList.add('modal-open');
-    const favoriteItemsTemp = getFavoriteFurnitureFromLocalStorage();
-    renderListFavoriteFurniture(filterFavoriteItems(favoriteItemsTemp));
-    removeFavorite(favoriteItems);
-  });
-};
-
-const handleAddEventListenerForCloseButton = () => {
-  modalClose.addEventListener('click', () => {
-    const showModal = document.getElementById('favorite-modal');
-    showModal.classList.remove('modal-open');
-  });
-};
-
-// Render favorites list
-const renderListFavoriteFurniture = (filterFavoriteList) => {
-  while (favoritesList.hasChildNodes()) {
-    favoritesList.removeChild(favoritesList.firstChild);
-  };
-  const listFavorite = filterFavoriteList;
-  listFavorite.forEach((furniture) => {
-    favoritesList.insertAdjacentHTML('beforeend', createElement(furniture));
-  });
 };
 
 // Add furniture item to list favorites
@@ -103,58 +57,37 @@ const addFavorite = () => {
   });
 };
 
-// Change icon-favorite with icon-trash
-const changeIcon = (node, type = 'add') => {
-  let typeObj = {};
-  if (type === 'add') {
-    typeObj = {
-      classNameIcon: '.icon-favorite',
-      img: '<img class="icon-trash" src="https://i.imgur.com/m4xuLMv.png" alt="Trash">',
-    };
-  } else {
-    typeObj = {
-      classNameIcon: '.icon-trash',
-      img: '<img class="icon-favorite" src="https://i.imgur.com/UnnmWMl.png" alt="Favorite">',
-    };
-  };
-  node.removeChild(node.querySelector(typeObj.classNameIcon));
-  node.insertAdjacentHTML('beforeend', typeObj.img);
-};
-
 // Remove furniture item from favorites list
-const btnRemoveFunc = (btn) => {
-  if (confirm("Are you sure you want to remove it from favorites list?")) {
-    const furnitureItem = btn.parentElement.parentElement;
-    const furnitureList = furnitureItem.parentElement;
-    const favoriteId = furnitureItem.getAttribute('data-id');
-    favoriteItems = favoriteItems.filter(item => item !== favoriteId);
-    setFavoriteFurnitureToLocalStorage(favoriteItems);
-    if (furnitureList.classList.contains('favorites-list')) {
-      furnitureItem.remove();
-      const selectedFurniture = document.querySelector(`[data-id='${favoriteId}'] button`);
-      changeIcon(selectedFurniture, 'remove');
-    } else {
-      changeIcon(btn, 'remove');
-    };
-  };
-};
+// const btnRemoveFunc = (btn) => {
+//   if (confirm("Are you sure you want to remove it from favorites list?")) {
+//     const furnitureItem = btn.parentElement.parentElement;
+//     const furnitureList = furnitureItem.parentElement;
+//     const favoriteId = furnitureItem.getAttribute('data-id');
+//     favoriteItems = favoriteItems.filter(item => item !== favoriteId);
+//     setFavoriteFurnitureToLocalStorage(favoriteItems);
+//     if (furnitureList.classList.contains('favorites-list')) {
+//       furnitureItem.remove();
+//       const selectedFurniture = document.querySelector(`[data-id='${favoriteId}'] button`);
+//       changeIcon(selectedFurniture, 'remove');
+//     } else {
+//       changeIcon(btn, 'remove');
+//     };
+//   };
+// };
 
-const removeFavorite = (favoriteItems) => {
-  const btnRemoveFavorite = document.querySelectorAll('.favorites-list .btn-favorite');
-  btnRemoveFavorite.forEach(btn => {
-    btn.addEventListener('click', () => {
-      btnRemoveFunc(btn, favoriteItems);
-    });
-  });
-};
+// const removeFavorite = (favoriteItems) => {
+//   const btnRemoveFavorite = document.querySelectorAll('.favorites-list .btn-favorite');
+//   btnRemoveFavorite.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//       btnRemoveFunc(btn, favoriteItems);
+//     });
+//   });
+// };
 
 window.addEventListener('DOMContentLoaded', () => {
-  loadData();
+  renderFurniture();
   // Add favorite
   addFavorite();
-  // Favorites List
-  handleAddEventListenerForFavoriteButton();
-  handleAddEventListenerForCloseButton();
   // Navigation bar
   handleAddEventListenerNavMenu();
 });
