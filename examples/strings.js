c58311df54d063e7ad386526e24b4e4debbd7d91
Kien Dinh <kien.dinh@asnet.com.vn>
2023-08-29 17:35:02 +0700

    feat: implement example for strings

---
+++ examples/strings.js
@@ -0,0 +1,9 @@
+// There are 3 ways to write a string '', "", ``
+const a = 'My name is Kien';
+const b = `Hello, ${a}`;
+console.log(b); // Hello, My name is Kien
+
+// String method:
+/// charAt(index)
+const charAtString = 'Kien';
+console.log(charAtString.charAt(3)); // n
