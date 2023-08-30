9d8cf49f695b29708f46943bc8c95245da58ca42
Kien Dinh <kien.dinh@asnet.com.vn>
2023-08-30 14:33:06 +0700

    feat: example for asynchronous

---
+++ examples/asynchronous.js
@@ -0,0 +1,22 @@
+/*
+ *  Example about Asynchronous
+ */
+const networkRequest = () => {
+  /*
+   * runs after 5 seconds
+   */
+  setTimeout(() => {
+    console.log('After'); // after
+  }, 5000);
+  /*
+   * runs after 2 seconds
+   */
+  setTimeout(() => {
+    console.log('Before'); // before
+  }, 2000);
+};
+/*
+ * run first
+ */
+console.log('Hello World'); // Hello world
+networkRequest();
