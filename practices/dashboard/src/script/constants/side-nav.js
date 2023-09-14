3f28ed6db0ae7c714808f10e334979d8536b4e3d
Kien Dinh <kien.dinh@asnet.com.vn>
2023-09-14 16:53:24 +0700

    feat: implement constants for side nav

---
+++ practices/dashboard/src/script/constants/side-nav.js
@@ -0,0 +1,44 @@
+const NAV_ITEMS = [
+  {
+    name: 'Dashboard',
+    img: './images/svg/keyboard-command.svg',
+    href: '/'
+  },
+  {
+    name: 'Users',
+    img: './images/svg/poll.svg',
+    href: '#'
+  },
+  {
+    name: 'Documents',
+    img: './images/svg/document-text.svg',
+    href: '#'
+  },
+  {
+    name: 'Photos',
+    img: './images/svg/gallery.svg',
+    href: '#'
+  },
+  {
+    name: 'Hierarchy',
+    img: './images/svg/hand-coin.svg',
+    href: '#'
+  },
+  {
+    name: 'Message',
+    img: './images/svg/message.svg',
+    href: '#'
+  },
+  {
+    name: 'Help',
+    img: './images/svg/help.svg',
+    href: '#'
+  },
+  {
+    name: 'Setting',
+    img: './images/svg/setting.svg',
+    href: '#'
+  }
+]
+
+export { NAV_ITEMS }
