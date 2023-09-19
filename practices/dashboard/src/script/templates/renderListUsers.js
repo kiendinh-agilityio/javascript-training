9fe54fb3dc4785ab6eb4664d4d0c70d020ebd6f8
Kien Dinh <kien.dinh@asnet.com.vn>
2023-09-19 12:29:01 +0700

    fet: update renderListUsers

--- practices/dashboard/src/script/templates/renderListUsers.js
+++ practices/dashboard/src/script/templates/renderListUsers.js
@@ -1,4 +1,5 @@
 import { getUserFromLocalStorage } from '../mocks/list-users'
+import { userItem } from './userItem'
 
 const generateListUsers = (users) => {
   return `
@@ -13,50 +14,8 @@ const generateListUsers = (users) => {
     </thead>
     <tbody>
       ${
-        (users || getUserFromLocalStorage).map((user) => {
-          const {
-            id,
-            lastName,
-            firstName,
-            email,
-            date,
-            phone,
-            role,
-            roleId
-          } = user
-          return `
-            <tr class="table-row" data-id=${id}>
-              <td class="dasboard-item">
-                <p class="user-name">${firstName} ${lastName}</p>
-                <p class="user-email">${email}</p>
-              </td>
-              <td class="table-cell">
-                <div class="d-flex justify-center items-center user-role-tag user-role-${roleId}">${role}</div>
-              </td>
-              <td class="table-cell create-date">${date}</td>
-              <td class="table-cell phone-number">${phone}</td>
-              <td class="table-cell">
-                <button id="btn-edit" class="btn-edit">
-                  <img
-                    loading="lazy"
-                    width="20px"
-                    height="20px"
-                    src="./images/svg/edit.svg"
-                    alt="Edit icon"
-                  />
-                </button>
-                <button id="btn-remove" class="btn-remove">
-                  <img
-                    loading="lazy"
-                    width="20px"
-                    height="20px"
-                    src="./images/svg/remove.svg"
-                    alt="Remove icon"
-                  />
-                </button>
-              </td>
-            </tr>
-          `
+        users.map((user) => {
+          return userItem(user)
         }).join('')
       }
     </tbody>
@@ -70,7 +29,7 @@ const generateUsersTable = (users) => {
   }
   console.log(users)
   const listUsers = document.getElementById('list-users')
-  listUsers.innerHTML = generateListUsers(users)
+  listUsers.innerHTML = generateListUsers(renderUsers)
 }
 
 export { generateUsersTable }
