564c963b4cc1c286910071c2fa21b81e0da5430f
Kien Dinh <kien.dinh@asnet.com.vn>
2023-09-19 15:27:55 +0700

    feat: update folder utils

--- practices/dashboard/src/script/utils/index.js
+++ practices/dashboard/src/script/utils/index.js
@@ -1,10 +1,1 @@
-export const checkFullNameMatch = (firstName, lastName, searchTerm) => {
-  return (firstName + ' ' + lastName).toLowerCase().includes(searchTerm)
-}
-export const checkEmailMatch = (email, searchTerm) => {
-  return email.toLowerCase().includes(searchTerm)
-}
-
-export const checkPhoneMatch = (phone, searchTerm) => {
-  return phone.includes(searchTerm)
-}
+export * from './user'
