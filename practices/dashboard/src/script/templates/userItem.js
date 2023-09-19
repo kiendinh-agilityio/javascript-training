ae41f4c8b015b27315e24226e1d960016284bc97
Kien Dinh <kien.dinh@asnet.com.vn>
2023-09-19 12:29:35 +0700

    feat: create userItem.js

---
+++ practices/dashboard/src/script/templates/userItem.js
@@ -0,0 +1,44 @@
+export const userItem = (user) => {
+  const {
+    id,
+    lastName,
+    firstName,
+    email,
+    date,
+    phone,
+    role,
+    roleId
+  } = user
+  return `<tr class="table-row" data-id=${id}>
+  <td class="dasboard-item">
+    <p class="user-name">${firstName} ${lastName}</p>
+    <p class="user-email">${email}</p>
+  </td>
+  <td class="table-cell">
+    <div class="d-flex justify-center items-center user-role-tag user-role-${roleId}">${role}</div>
+  </td>
+  <td class="table-cell create-date">${date}</td>
+  <td class="table-cell phone-number">${phone}</td>
+  <td class="table-cell">
+    <button id="btn-edit" class="btn-edit">
+      <img
+        loading="lazy"
+        width="20px"
+        height="20px"
+        src="./images/svg/edit.svg"
+        alt="Edit icon"
+      />
+    </button>
+    <button id="btn-remove" class="btn-remove">
+      <img
+        loading="lazy"
+        width="20px"
+        height="20px"
+        src="./images/svg/remove.svg"
+        alt="Remove icon"
+      />
+    </button>
+  </td>
+</tr>
+`
+}
