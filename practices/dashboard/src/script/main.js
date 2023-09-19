2272a88a2bc27407cc0fc7419d949241bf44dd0a
Kien Dinh <kien.dinh@asnet.com.vn>
2023-09-19 15:28:33 +0700

    feat: update the feature for search users

--- practices/dashboard/src/script/main.js
+++ practices/dashboard/src/script/main.js
@@ -1,26 +1,29 @@
 import { renderSideNav } from './side-nav/renderSideNav'
 import { generateUsersTable } from './templates/renderListUsers'
 import { getUserFromLocalStorage } from './mocks/list-users'
-import { searchInput, searchButton } from './dom'
-import { checkFullNameMatch, checkEmailMatch, checkPhoneMatch } from './utils/index'
+import { isFullNameMatch, isEmailMatch, isPhoneMatch } from './utils'
+
+// Variables scope
+const searchInput = document.getElementById('search-input')
+const searchButton = document.getElementById('search-button')
 
 // Handle the search when users the search button is pressed
 searchButton.addEventListener('click', () => {
   const searchTerm = searchInput.value.toLowerCase().trim()
   // Search in the list users
   const searchResults = getUserFromLocalStorage.filter(user => {
-    const nameMatch = checkFullNameMatch(user.firstName, user.lastName, searchTerm)
-    const emailMatch = checkEmailMatch(user.email, searchTerm)
-    const phoneMatch = checkPhoneMatch(user.phone) // Compare phone numbers without converting to lowercase
+    const nameMatch = isFullNameMatch(user.firstName, user.lastName, searchTerm)
+    const emailMatch = isEmailMatch(user.email, searchTerm)
+    const phoneMatch = isPhoneMatch(user.phone) // Compare phone numbers without converting to lowercase
     return nameMatch || emailMatch || phoneMatch
   })
   generateUsersTable(searchResults)
-  console.log(searchResults)
 })
 
 // Handle searches as the user types and presses Enter
-searchInput.addEventListener('keyup', event => {
+searchInput.addEventListener('keypress', event => {
   if (event.key === 'Enter') {
+    event.preventDefault()
     searchButton.click() // Activate the search button when pressing Enter
   }
 })
