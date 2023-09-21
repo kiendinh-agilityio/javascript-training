40fb7175e8ea4423296ccc28047fc5afd2b1d8c5
Kien Dinh <kien.dinh@asnet.com.vn>
2023-09-21 15:47:08 +0700

    feat: create file validateUserForm

---
+++ practices/dashboard/src/script/validate/validateUserForm.js
@@ -0,0 +1,45 @@
+import { REGEX_EMAIL, REGEX_PHONE, REGEX_NAME, VALIDATE_MESSAGE } from '../constants'
+
+const { inValidEmail, invalidPhone, inValidUsername, requiredError } = VALIDATE_MESSAGE
+
+export const validateUserForm = (user) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    role
+  } = user
+
+  const errors = {}
+
+  if (!firstName) {
+    errors.firstName = `${requiredError.replace('{field}', 'First Name')}`
+  } else if (!REGEX_NAME(firstName)) {
+    errors.firstName = `${inValidUsername}`
+  }
+
+  if (!lastName) {
+    errors.lastName = `${requiredError.replace('{field}', 'Last Name')}`
+  } else if (!REGEX_NAME(lastName)) {
+    errors.lastName = `${inValidUsername}`
+  }
+
+  if (!email) {
+    errors.email = `${requiredError.replace('{field}', 'Email')}`
+  } else if (!REGEX_EMAIL(email)) {
+    errors.email = `${inValidEmail}`
+  }
+
+  if (!phone) {
+    errors.phone = `${requiredError.replace('{field}', 'Phone Number')}`
+  } else if (!REGEX_PHONE(phone)) {
+    errors.phone = `${invalidPhone}`
+  }
+
+  if (!role) {
+    errors.role = `${requiredError.replace('{field}', 'Role Type')}`
+  }
+
+  return errors
+}
