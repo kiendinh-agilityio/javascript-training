2f99256e90cdaa0d096b69e23c7ca1aeeed941eb
Kien Dinh <kien.dinh@asnet.com.vn>
2023-09-21 15:45:59 +0700

    feat: create messsage.js

---
+++ practices/dashboard/src/script/constants/message.js
@@ -0,0 +1,7 @@
+export const VALIDATE_MESSAGE = {
+  requiredError: '{field} is required',
+  inValidEmail: 'Invalid email. Please enter a valid email address',
+  inValidUsername:
+    'Please enter a minimum of 6 and a maximum of 30 characters for the username',
+  invalidPhone: 'Invalid phone number. Please enter a valid phone number. Example: 202-555-0192'
+}
