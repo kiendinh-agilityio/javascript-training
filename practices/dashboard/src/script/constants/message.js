56344e019711a426d82287dd136eaac4e5aaa03c
Kien Dinh <kien.dinh@asnet.com.vn>
2023-09-25 16:55:52 +0700

    feat: update message.js

--- practices/dashboard/src/script/constants/message.js
+++ practices/dashboard/src/script/constants/message.js
@@ -1,7 +1,7 @@
 export const VALIDATE_MESSAGE = {
-  requiredError: '{field} is required',
-  inValidEmail: 'Invalid email. Please enter a valid email address',
-  inValidUsername:
+  REQUIRED_ERROR: '{field} is required',
+  INVALID_EMAIL: 'Invalid email. Please enter a valid email address',
+  INVALID_NAME:
     'Please enter a minimum of 4 and a maximum of 20 characters for the {field}',
-  invalidPhone: 'Invalid phone number. Please enter a valid phone number. Example: 202-555-0192'
+  INVALID_PHONE: 'Invalid phone number. Please enter a valid phone number. Example: 202-555-0192'
 }
