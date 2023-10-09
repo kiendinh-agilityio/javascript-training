e7361e7a1490fa3f7ebf862cb40e0c4a1e202aaa
Kien Dinh <kien.dinh@asnet.com.vn>
2023-10-09 23:06:06 +0700

    feat: update code for regex

--- practices/dashboard/src/script/constants/regex.js
+++ practices/dashboard/src/script/constants/regex.js
@@ -7,7 +7,7 @@ export const REGEX_PATTERN = {
 export const REGEX_EMAIL = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
 
 // check the character must match the phone number format. Example: 202-555-0192
-export const REGEX_PHONE = /^\d{3,}-\d{3}-\d{4}$/
+export const REGEX_PHONE = /^\d{3,}\d{3}\d{4}$/
 
 // check characters with strings or numbers between 6 and 30 characters in length
 export const REGEX_NAME = /[0-9a-zA-Z]{4,20}/
