ec53a9ec9b36429d50869b149fc27f0031e2478b
Kien Dinh <kien.dinh@asnet.com.vn>
2023-10-17 09:28:35 +0700

    feat: update main.js

--- practices/dashboard/src/script/main.js
+++ practices/dashboard/src/script/main.js
@@ -1,6 +1,6 @@
 import { renderSideNav } from './side-nav/renderSideNav';
 import { generateUsersTable } from './templates/renderListUsers';
-import { getUserFromLocalStorage } from './mocks/list-users';
+import { getUserFromLocalStorage } from './mocks/listUsers';
 import { eventLoader } from './events/index';
 
 const InitialApp = () => {
