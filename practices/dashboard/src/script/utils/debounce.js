1195bc44cb5622dd0568f597e2cf2ecd3e1fa4f7
Kien Dinh <kien.dinh@asnet.com.vn>
2023-10-18 15:44:57 +0700

    feat: update function debounce

--- practices/dashboard/src/script/utils/debounce.js
+++ practices/dashboard/src/script/utils/debounce.js
@@ -1,11 +1,11 @@
 export const debounce = (func, delay) => {
   let timeoutId;
-  return function () {
-    const context = this;
-    const args = arguments;
+
+  return function (...args) {
     clearTimeout(timeoutId);
+
     timeoutId = setTimeout(() => {
-      func.apply(context, args);
+      func.apply(this, args);
     }, delay);
   };
 };
