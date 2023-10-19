206b89f2aa3d79816e0874aa539778b67684a593
Kien Dinh <kien.dinh@asnet.com.vn>
2023-10-19 15:33:31 +0700

    feat: move formattedDate from constants to utils

--- practices/dashboard/src/script/utils/format.js
+++ practices/dashboard/src/script/utils/format.js
@@ -1,3 +1,6 @@
+import { MONTHS } from '../constants/index';
+
+// Format Phone Number
 const formatPhoneNumber = (phoneNumber) => {
   if (phoneNumber.length >= 10) {
     return `(${phoneNumber.slice(0, 3)})-${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6, 10)}`;
@@ -17,3 +20,29 @@ export const formatLimitedPhoneNumberInput = (event) => {
     event.target.value = formatPhoneNumber(cleanPhoneNumber);
   }
 };
+
+// Format date
+const getShortMonthName = (date) => {
+  return MONTHS[date.getMonth()];
+};
+
+const addDaySuffix = (day) => {
+  if (day >= 11 && day <= 13) {
+    return day + 'th';
+  }
+  switch (day % 10) {
+    case 1:
+      return day + 'st';
+    case 2:
+      return day + 'nd';
+    case 3:
+      return day + 'rd';
+    default:
+      return day + 'th';
+  }
+};
+
+const currentDate = new Date();
+const day = addDaySuffix(currentDate.getDate());
+
+export const formattedDate = `${getShortMonthName(currentDate)} ${day}, ${currentDate.getFullYear()}`;
