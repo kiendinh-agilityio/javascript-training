11d8b2f9ebf49a442c0cda7b562b12f175cdeea0
Kien Dinh <kien.dinh@asnet.com.vn>
2023-10-20 18:44:59 +0700

    feat: update file validateUserForm

--- practices/dashboard/src/script/validate/validateUserForm.js
+++ practices/dashboard/src/script/validate/validateUserForm.js
@@ -8,39 +8,56 @@ import {
 
 const { REQUIRED_ERROR } = VALIDATE_MESSAGE;
 
+const validateNameField = (value, fieldName) => {
+  return (
+    requiredMessage(value, fieldName) || displayWarningName(value, fieldName)
+  );
+};
+
+const validateEmailField = (email) => {
+  return requiredMessage(email, 'Email') || displayWarningEmail(email);
+};
+
+const validatePhoneField = (phone) => {
+  return requiredMessage(phone, 'Mobile No') || displayWarningPhone(phone);
+};
+
+const validateRoleField = (role) => {
+  if (!role) {
+    return REQUIRED_ERROR.replace('{field}', 'Role Type');
+  }
+
+  return null;
+};
+
 export const validateUserForm = (user) => {
   const { firstName, lastName, email, phone, role } = user;
 
   const errors = {};
 
-  const firstNameError =
-    requiredMessage(firstName, 'First Name') ||
-    displayWarningName(firstName, 'First Name');
+  const firstNameError = validateNameField(firstName, 'First Name');
   if (firstNameError) {
     errors.firstName = firstNameError;
   }
 
-  const lastNameError =
-    requiredMessage(lastName, 'Last Name') ||
-    displayWarningName(lastName, 'Last Name');
+  const lastNameError = validateNameField(lastName, 'Last Name');
   if (lastNameError) {
     errors.lastName = lastNameError;
   }
 
-  const emailError =
-    requiredMessage(email, 'Email') || displayWarningEmail(email);
+  const emailError = validateEmailField(email);
   if (emailError) {
     errors.email = emailError;
   }
 
-  const phoneError =
-    requiredMessage(phone, 'Mobile No') || displayWarningPhone(phone);
+  const phoneError = validatePhoneField(phone);
   if (phoneError) {
     errors.phone = phoneError;
   }
 
-  if (!role) {
-    errors.role = `${REQUIRED_ERROR.replace('{field}', 'Role Type')}`;
+  const roleError = validateRoleField(role);
+  if (roleError) {
+    errors.role = roleError;
   }
 
   return errors;
