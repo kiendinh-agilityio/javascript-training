9f54e9372a4e5f6968ceff01e7bbd05a02f0b798
Kien Dinh <kien.dinh@asnet.com.vn>
2023-10-20 18:44:45 +0700

    feat: update file index.js of folder untils

--- practices/dashboard/src/script/utils/index.js
+++ practices/dashboard/src/script/utils/index.js
@@ -3,3 +3,6 @@ export * from './debounce';
 export * from './format';
 export * from './loadingSpinner';
 export * from './camelToHyphen';
+export * from './emptyResultSearch';
+export * from './toastMessage';
+export * from './trimmingString';
