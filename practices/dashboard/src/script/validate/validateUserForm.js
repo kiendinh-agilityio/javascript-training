2d025d94a7e98b8829da4b45608fb2804d389695
Kien Dinh <kien.dinh@asnet.com.vn>
2023-10-23 11:27:38 +0700

    feat: update validateUserForm

--- practices/dashboard/src/script/validate/validateUserForm.js
+++ practices/dashboard/src/script/validate/validateUserForm.js
@@ -1,34 +1,9 @@
-import { VALIDATE_MESSAGE } from '../constants';
 import {
-  displayWarningName,
-  displayWarningEmail,
-  displayWarningPhone,
-  requiredMessage,
+  validateNameField,
+  validateEmailField,
+  validatePhoneField,
+  validateRoleField,
 } from './validations';
-
-const { REQUIRED_ERROR } = VALIDATE_MESSAGE;
-
-const validateNameField = (value, fieldName) => {
-  return (
-    requiredMessage(value, fieldName) || displayWarningName(value, fieldName)
-  );
-};
-
-const validateEmailField = (email) => {
-  return requiredMessage(email, 'Email') || displayWarningEmail(email);
-};
-
-const validatePhoneField = (phone) => {
-  return requiredMessage(phone, 'Mobile No') || displayWarningPhone(phone);
-};
-
-const validateRoleField = (role) => {
-  if (!role) {
-    return REQUIRED_ERROR.replace('{field}', 'Role Type');
-  }
-
-  return null;
-};
 
 export const validateUserForm = (user) => {
   const { firstName, lastName, email, phone, role } = user;
