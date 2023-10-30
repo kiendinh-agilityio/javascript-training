07f4cd0e30be8fe02fd7cc7660a58e71f1e16e58
Kien Dinh <kien.dinh@asnet.com.vn>
2023-10-30 17:20:54 +0700

    feat: implement ui for sign up

--- src/scripts/pages/authen.js
+++ src/scripts/pages/authen.js
@@ -1,4 +1,12 @@
 import { handleTogglePassword } from '../utils/index';
+import { AuthenController } from '../controllers/authen';
+import { AuthenView } from '../views/authen';
 
-// // Call the function to handle the show password
+const authenPage = () => {
+  new AuthenController(new AuthenView());
+};
+
+authenPage();
+
+// Call the function to handle the show password
 handleTogglePassword();
