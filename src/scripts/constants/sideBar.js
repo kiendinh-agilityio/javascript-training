6fc3e0492a80d7a6d3001d45196704b7367055b7
Kien Dinh <kien.dinh@asnet.com.vn>
2023-10-31 18:56:17 +0700

    feat: create sidebar list

---
+++ src/scripts/constants/sideBar.js
@@ -0,0 +1,77 @@
+export const MAIN_SIDEBAR_ITEMS = [
+  {
+    URL: '/images/svg/folder.svg',
+    ALT: 'Folder icon',
+  },
+  {
+    URL: '/images/svg/loudspeaker-green.svg',
+    ALT: 'Loudspeaker icon',
+  },
+  {
+    URL: '/images/svg/s-pulse.svg',
+    ALT: 'S-pulse icon',
+  },
+  {
+    URL: '/images/svg/bolt.svg',
+    ALT: 'Bolt icon',
+  },
+  {
+    URL: '/images/svg/input.svg',
+    ALT: 'Input icon',
+  },
+];
+
+export const SUB_SIDEBAR_ITEMS = [
+  {
+    TEXT: 'FAVOURITES',
+  },
+  {
+    URL: '/images/svg/favorite.svg',
+    ALT: 'Favorite icon',
+    TEXT: 'Waterloo',
+  },
+  {
+    URL: '/images/svg/favorite.svg',
+    ALT: 'Favorite icon',
+    TEXT: 'Austerlitz',
+    ACTIVE: true,
+  },
+  {
+    TEXT: 'ACTIVE',
+  },
+  {
+    URL: '/images/svg/loudspeaker.svg',
+    ALT: 'Loudspeaker icon',
+    TEXT: 'Anais',
+  },
+  {
+    URL: '/images/svg/loudspeaker.svg',
+    ALT: 'Loudspeaker icon',
+    TEXT: 'Minimal apps',
+  },
+  {
+    URL: '/images/svg/loudspeaker.svg',
+    ALT: 'Loudspeaker icon',
+    TEXT: 'Bentley',
+  },
+  {
+    URL: '/images/svg/loudspeaker.svg',
+    ALT: 'Loudspeaker icon',
+    TEXT: 'Aston Martin',
+  },
+  {
+    URL: '/images/svg/loudspeaker.svg',
+    ALT: 'Loudspeaker icon',
+    TEXT: 'Rolex',
+  },
+  {
+    URL: '/images/svg/loudspeaker.svg',
+    ALT: 'Loudspeaker icon',
+    TEXT: 'Dugarry',
+  },
+  {
+    URL: '/images/svg/loudspeaker.svg',
+    ALT: 'Loudspeaker icon',
+    TEXT: 'Obama',
+  },
+];
