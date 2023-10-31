f3635dea1e7d6af9f12fbcf6eae54eff2901bb76
Kien Dinh <kien.dinh@asnet.com.vn>
2023-10-31 14:22:16 +0700

    feat: update for handleTogglePassword

--- src/scripts/utils/handleTogglePassword.js
+++ src/scripts/utils/handleTogglePassword.js
@@ -1,19 +1,11 @@
-export const handleTogglePassword = () => {
-  const togglePasswordButtons = document.querySelectorAll('.toggle-password');
+export const handleTogglePassword = (togglePasswordButton) => {
+  const inputField = togglePasswordButton.parentElement.querySelector('.show-password');
+  const eyeImage = togglePasswordButton.querySelector('img');
+  const isShow = inputField.getAttribute('data-show-password') === 'show';
 
-  togglePasswordButtons.forEach((togglePasswordButton) => {
-    togglePasswordButton.addEventListener('click', () => {
-      const inputField = togglePasswordButton.parentElement.querySelector('.show-password');
-      const eyeImage = togglePasswordButton.querySelector('img');
-      const isShow = inputField.getAttribute('data-is-show') === 'true';
+  const eyeImageSrc = isShow ? '/images/svg/eye-key-password.svg' : '/images/svg/eye-password.svg';
+  eyeImage.src = eyeImageSrc;
 
-      // Change eye image
-      const eyeImageSrc = isShow ? '/images/svg/eye-key-password.svg' : '/images/svg/eye-password.svg';
-      eyeImage.src = eyeImageSrc;
-
-      // Reverse the display style of the password input field
-      inputField.type = isShow ? 'password' : 'text';
-      inputField.setAttribute('data-is-show', isShow ? 'false' : 'true');
-    });
-  });
+  inputField.type = isShow ? 'password' : 'text';
+  inputField.setAttribute('data-show-password', isShow ? 'hidden' : 'show');
 };
