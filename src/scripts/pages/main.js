2138455886be6231d51b716a5746b7371522a25f
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-02 17:05:34 +0700

    feat: implement modal ads for home page

--- src/scripts/pages/main.js
+++ src/scripts/pages/main.js
@@ -1,3 +1,11 @@
 import { initializeSidebar } from '../utils/index';
+import { AdsController } from '../controllers/index';
+import { AdsView } from '../views/index';
+import { AdsModel } from '../models/index';
 
+const homePage = () => {
+  new AdsController(new AdsModel(), new AdsView());
+};
+
+homePage();
 initializeSidebar();
