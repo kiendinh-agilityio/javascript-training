d863402a699899878dfd42a04ed8dc55e5bfc8ca
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-06 16:32:37 +0700

    feat: create message constants

---
+++ src/scripts/constants/message.js
@@ -0,0 +1,5 @@
+export const VALIDATE_MESSAGE = {
+  REQUIRED_ERROR: '{field} is required',
+  INVALID_EMAIL: 'Invalid email. Please enter a valid email address',
+  INVALID_PASSWORD: 'Invalid password. Password must have at least 8 characters',
+};
