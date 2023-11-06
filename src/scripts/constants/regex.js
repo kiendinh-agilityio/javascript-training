1953351b97703493a00011c786aae6a19fb5f77e
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-06 17:28:23 +0700

    feat: update regex constants

--- src/scripts/constants/regex.js
+++ src/scripts/constants/regex.js
@@ -1,7 +1,3 @@
-export const REGEX_PATTERN = {
-  CAMEL_CASE_SEPARATOR: /([a-z])([A-Z])/g,
-};
-
 // Check email have @ symbol, a string precedes it and the following string needs to contain a period, followed by 2-3 characters.
 export const REGEX_EMAIL = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
