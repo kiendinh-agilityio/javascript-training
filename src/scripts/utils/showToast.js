7d6a29832d5e04089bbe27e4c8d2348013c7bb4a
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-06 16:33:49 +0700

    feat: create utils showToast

---
+++ src/scripts/utils/showToast.js
@@ -0,0 +1,27 @@
+// Function to create toast container
+const createToastContainer = (message, icon, isSuccess) => {
+  const toastContainer = document.createElement('div');
+
+  const toastTextClass = isSuccess ? 'toast-text-success' : 'toast-text-error';
+
+  toastContainer.innerHTML = `
+      <div class="toast items-center">
+        <img width="30px" height="30px" src="../images/svg/${icon}" alt="${icon}">
+        <p class="toast-text ${toastTextClass}">${message}</p>
+      </div>
+  `;
+  return toastContainer;
+};
+
+// Function to display toast
+export const showToast = (message, icon, isSuccess) => {
+  const toastContainer = createToastContainer(message, icon, isSuccess);
+  document.body.appendChild(toastContainer);
+
+  toastContainer.style.display = 'flex';
+
+  setTimeout(() => {
+    toastContainer.style.display = 'none';
+    document.body.removeChild(toastContainer);
+  }, 500);
+};
