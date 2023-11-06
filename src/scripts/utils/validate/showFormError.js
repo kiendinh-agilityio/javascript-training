f9a209bb43a891a4d0142048933dcbeff36d2917
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-06 17:31:01 +0700

    feat: update showFormError

--- src/scripts/utils/validate/showFormError.js
+++ src/scripts/utils/validate/showFormError.js
@@ -1,12 +1,6 @@
-import { REGEX_PATTERN } from '../../constants/index';
-
-const camelCaseToHyphenCase = (input) =>
-  input.replace(REGEX_PATTERN.CAMEL_CASE_SEPARATOR, '$1-$2').toLowerCase();
-
 const updateErrorMessages = (errors) => {
   Object.entries(errors).forEach(([key, value]) => {
-    const newKey = camelCaseToHyphenCase(key);
-    const target = document.getElementById(`${newKey}-error`);
+    const target = document.getElementById(`${key}-error`);
     if (target) {
       target.innerText = value;
     }
