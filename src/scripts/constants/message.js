2ea2d1ef3c09ccf98e4c63aac5fe8cab6984705e
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-07 17:44:56 +0700

    feat: update constants for message

--- src/scripts/constants/message.js
+++ src/scripts/constants/message.js
@@ -2,10 +2,19 @@ export const VALIDATE_MESSAGE = {
   REQUIRED_ERROR: '{field} is required',
   INVALID_EMAIL: 'Invalid email. Please enter a valid email address',
   INVALID_PASSWORD: 'Invalid password. Password must have at least 8 characters',
+  INVALID_CONFIRM_PASSWORD: 'Password and Confirm Password do not match.',
 };
 
 // Message Login
 export const LOGIN_MESSAGE = {
   SUCCESS: 'Sign in successfully',
   UNSUCCESSFUL: 'Sign in unsuccessful',
+  EMPTY: 'Please enter both email and password.',
+};
+
+// Message Signup
+export const SIGNUP_MESSAGE = {
+  SUCCESS: 'Registration successful. You can now sign in.',
+  EMAIL: 'Email already exists. Please use a different email.',
+  EMPTY: 'Email, password, and confirm password are required.',
 };
