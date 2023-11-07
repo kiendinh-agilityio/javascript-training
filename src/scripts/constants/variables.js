c9f44d48b5120d6440baf03bf67d0f382dd0db69
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-07 17:44:45 +0700

    feat: update constants for variables

--- src/scripts/constants/variables.js
+++ src/scripts/constants/variables.js
@@ -3,6 +3,7 @@ export const DISPLAY_CLASS = {
   FLEX: 'flex',
   HIDDEN: 'none',
   BLOCK: 'block',
+  ACTIVE: 'active',
 };
 
 // Title Modal
@@ -11,7 +12,13 @@ export const TITLE_MODAL = {
   EDIT: 'Edit Ads',
 };
 
-// END_POINT
-export const END_POINT = {
+// Endpoint name
+export const END_POINT_NAMES = {
   USERS: 'users',
+};
+
+// Auth Form Title
+export const TITLE_AUTHEN_PAGE = {
+  LOGIN: 'Sign In',
+  REGISTER: 'Create New Account',
 };
