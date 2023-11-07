f21fc1dcd7721930257c02a121368ed8be2b6eb4
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-07 21:37:42 +0700

    feat: update controllers for the feature logout

--- src/scripts/controllers/index.js
+++ src/scripts/controllers/index.js
@@ -1,6 +1,15 @@
 export class AdsController {
   constructor(model, view) {
+    // Initialize the model and view
     this.model = model;
     this.view = view;
+
+    // Assign the logout handler from the view
+    this.view.setLogoutHandler(this.handleLogout.bind(this));
+  }
+
+  // Handle the logout action
+  handleLogout() {
+    window.location.href = 'authen.html';
   }
 }
