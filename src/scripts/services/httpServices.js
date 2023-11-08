31ca4f6a3664dd18354b0019be4ad092e7fbe7d1
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-08 20:12:00 +0700

    feat: update code for httpServices

--- src/scripts/services/httpServices.js
+++ src/scripts/services/httpServices.js
@@ -1,95 +1,74 @@
 import { BASE_API, END_POINTS, METHOD_API } from '../constants/index';
 
+/**
+ * A reusable function for making HTTP requests.
+ * @param {string} url - The URL for the API endpoint.
+ * @param {string} method - The HTTP method (GET, POST, PUT, DELETE).
+ * @param {object} data - The data to be sent in the request body (optional).
+ * @returns {Promise} A promise that resolves to the JSON response data or an error.
+ */
+const sendRequest = async (url, method, data) => {
+  try {
+    const response = await fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data ? JSON.stringify(data) : null,
+    });
+
+    // Check if the response status is not okay (e.g., non-2xx status code)
+    if (!response.ok) {
+      throw new Error('Request failed');
+    }
+
+    // Parse and return the JSON response data
+    return await response.json();
+  } catch (error) {
+    return error; // Return the error in case of any issues
+  }
+};
+
 export const httpServices = () => {
   return {
     /**
      * Fetch a list of advertisements from the API.
-     * @returns A data containing the list of advertisements.
+     * @returns {Promise} A promise that resolves to the list of advertisements.
      */
     async get() {
-      try {
-        const url = `${BASE_API}${END_POINTS.ADS}`;
-        const response = await fetch(url, {
-          method: METHOD_API.GET,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        return error;
-      }
+      const url = `${BASE_API}${END_POINTS.ADS}`;
+      return sendRequest(url, METHOD_API.GET);
     },
 
     /**
      * Add a new advertisement to the API.
-     * @param {Object} data - The data of the advertisement to be added.
-     * @returns A data containing the newly added advertisement.
+     * @param {object} data - The data of the advertisement to be added.
+     * @returns {Promise} A promise that resolves to the newly added advertisement.
      */
     async post(data) {
-      try {
-        const url = `${BASE_API}${END_POINTS.ADS}`;
-        const response = await fetch(url, {
-          method: METHOD_API.POST,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-        });
-
-        const responseData = await response.json();
-        return responseData;
-      } catch (error) {
-        return error;
-      }
+      const url = `${BASE_API}${END_POINTS.ADS}`;
+      return sendRequest(url, METHOD_API.POST, data);
     },
 
     /**
      * Update the information of an advertisement using the PUT method.
      * @param {number} id - The ID of the advertisement to be updated.
-     * @param {Object} data - The updated data for the advertisement.
-     * @returns Advertising data has been updated.
+     * @param {object} data - The updated data for the advertisement.
+     * @returns {Promise} A promise that resolves to the updated advertising data.
      */
     async put(id, data) {
-      try {
-        const url = `${BASE_API}${END_POINTS.ADS}${id}`;
-        const response = await fetch(url, {
-          method: METHOD_API.PUT,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-        });
-
-        const responseData = await response.json();
-        return responseData;
-      } catch (error) {
-        return error;
-      }
+      const url = `${BASE_API}${END_POINTS.ADS}${id}`;
+      return sendRequest(url, METHOD_API.PUT, data);
     },
 
     /**
      * Delete an advertisement by its ID using the DELETE method.
      * @param {number} id - The ID of the advertisement to be deleted.
-     * @returns A data containing the deleted advertisement.
+     * @returns {Promise} A promise that resolves to the deleted advertisement.
      */
     async delete(id) {
-      try {
-        const url = `${BASE_API}${END_POINTS.ADS}${id}`;
-        const response = await fetch(url, {
-          method: METHOD_API.DELETE,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-
-        const responseData = await response.json();
-        return responseData;
-      } catch (error) {
-        return error;
-      }
+      const url = `${BASE_API}${END_POINTS.ADS}${id}`;
+      return sendRequest(url, METHOD_API.DELETE);
     },
   };
 };
