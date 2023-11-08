a0b8a29f96ef55fa20ce9babf79ffc6e4af34d83
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-08 14:23:05 +0700

    feat: update models for home page

--- src/scripts/models/index.js
+++ src/scripts/models/index.js
@@ -1,9 +1,28 @@
+import { httpServices } from '../services/httpServices';
+
+/**
+ * Represents the AdsModel class for managing advertisement data.
+ */
 export class AdsModel {
   constructor() {
-    this.network = '';
-    this.link = '';
-    this.email = '';
-    this.phone = '';
-    this.status = '';
+    // Initialize the advertisement data and error state.
+    this.adsData = [];
+    this.error = null;
+  }
+
+  /**
+   * Asynchronously fetches advertisement data from an HTTP service.
+   * @returns {Promise} A promise that resolves with the HTTP response or rejects with an error.
+   */
+  async fetchAdsData() {
+    try {
+      // Send an HTTP GET request to retrieve advertisement data.
+      const response = await httpServices().get();
+      this.adsData = response.data;
+      return response;
+    } catch (error) {
+      // Handle errors by storing the error in the model.
+      this.error = error;
+    }
   }
 }
