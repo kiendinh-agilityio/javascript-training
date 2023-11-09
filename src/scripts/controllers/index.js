2ead0d4454a4b30363cb377d706ed231b41a853a
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-09 15:44:18 +0700

    feat: update controllers for feature search

--- src/scripts/controllers/index.js
+++ src/scripts/controllers/index.js
@@ -1,40 +1,106 @@
+import { debounce } from '../utils/debounce';
+
 /**
- * Represents the AdsController class for managing the interaction between the AdsModel and AdsView.
+ * Represents the AdsController class for handling the business logic and user interactions.
  */
 export class AdsController {
   constructor(model, view) {
-    // Initialize the model and view
     this.model = model;
     this.view = view;
     this.initialize();
 
-    // Assign the logout handler from the view
+    // Set logout handler for the view
     this.view.setLogoutHandler(this.handleLogout.bind(this));
+
+    // Add event listeners for search and clear search buttons
+    this.view.searchButton.addEventListener('click', this.handleSearch.bind(this));
+    this.view.btnClearSearch.addEventListener('click', this.handleClearSearch.bind(this));
+
+    // Initialize debounced search handling
+    this.handleSearchDebounced = debounce(this.handleSearch.bind(this), 500);
+
+    // Add event listeners for real-time search
+    this.view.searchInput.addEventListener('input', () => {
+      this.handleSearchDebounced();
+    });
+
+    // Add event listener for pressing Enter key in the search input
+    this.view.searchInput.addEventListener('keypress', (event) => {
+      if (event.key === 'Enter') {
+        this.handleSearch();
+      }
+    });
   }
 
   /**
-   * Handles the logout action by redirecting to the 'authen.html' page.
+   * Handles the logout action by redirecting to 'authen.html'.
    */
   handleLogout() {
     window.location.href = 'authen.html';
   }
 
   /**
-   * Initializes the controller by fetching ads data and updating the view.
+   * Initializes the AdsController and fetches the initial data.
    */
   async initialize() {
     try {
       const data = await this.model.fetchAdsData();
+
+      // Display error if there's an issue with data retrieval
       if (this.model.error) {
-        // Display an error in the view if there's an error in the model.
         this.view.displayError(this.model.error);
       } else {
-        // Display the ads list in the view.
         this.view.displayAdsList(data);
       }
     } catch (error) {
-      // Handle any unexpected errors and display them in the view.
       this.view.displayError(error);
     }
+  }
+
+  /**
+   * Handles the search action.
+   */
+  async handleSearch() {
+    const keyword = this.view.searchInput.value.trim().toLowerCase();
+    try {
+      if (keyword) {
+        if (this.model.adsData.length === 0) {
+          // Fetch data if it hasn't been loaded yet
+          await this.model.fetchAdsData();
+          if (this.model.error) {
+            this.view.displayError(this.model.error);
+            return;
+          }
+        }
+
+        // Filter the adsData based on the keyword entered in the search input.
+        const filteredAds = this.model.adsData.filter((item) => {
+          const { network, link, email, phone } = item;
+          const lowercaseKeyword = keyword.toLowerCase();
+
+          return network.includes(lowercaseKeyword) || email.includes(lowercaseKeyword) || phone.includes(lowercaseKeyword) || link.includes(lowercaseKeyword);
+        });
+
+        if (filteredAds.length > 0) {
+          // If there are results, display the matching ads list
+          this.view.displayAdsList(filteredAds);
+        } else {
+          // If no results, display a message
+          this.view.handleNoSearchResults();
+        }
+      } else {
+        // If the search input is empty, return to the initial state
+        this.initialize();
+      }
+    } catch (error) {
+      this.view.displayError(error);
+    }
+  }
+
+  /**
+   * Handles clearing the search input and displaying the initial data.
+   */
+  handleClearSearch() {
+    this.initialize();
   }
 }
