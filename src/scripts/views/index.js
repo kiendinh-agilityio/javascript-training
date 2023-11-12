41fac0c20575136f4168d1c03796554b5b74308c
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-12 15:28:18 +0700

    feat: update view for feature add ads

--- src/scripts/views/index.js
+++ src/scripts/views/index.js
@@ -1,5 +1,12 @@
-import { generateModalAds, toggleDropdown } from '../utils/index';
-import { DISPLAY_CLASS, TITLE_MODAL, MESSAGE } from '../constants/index';
+import {
+  generateModalAds,
+  toggleDropdown,
+  formatLimitedPhoneNumberInput,
+  StrimmingString,
+  validateAdsForm,
+  showFormErrors,
+} from '../utils/index';
+import { DISPLAY_CLASS, TITLE_MODAL, MESSAGE, PROFILE_ADS, ELEMENT_ID } from '../constants/index';
 import { generateListAds } from '../templates/generateAdsList';
 import { adsSearchElement } from '../dom/index';
 
@@ -9,6 +16,7 @@ export class AdsView {
     this.initEventListenersAds();
     this.initializeSearchInput();
     this.deleteHandler = null; // Track the delete handler
+    this.addAdsHandler = null;
   }
 
   /**
@@ -32,7 +40,7 @@ export class AdsView {
    * Initializes event listeners for AdsView.
    */
   initEventListenersAds() {
-    this.btnAdd.addEventListener('click', this.showAddAdsModal.bind(this));
+    this.btnAdd.addEventListener('click', this.showAdsModal.bind(this));
     this.modalAds.addEventListener('click', (event) => {
       if (event.target === this.modalAds) {
         this.closeModalHandler();
@@ -52,7 +60,7 @@ export class AdsView {
 
         // Show modal or perform other actions based on adsId
         this.showDeleteModal(adsId);
-        this.bindDeleteUserHandler(adsId);
+        this.bindDeleteAdsHandler(adsId);
       }
     });
 
@@ -78,6 +86,11 @@ export class AdsView {
         this.hideDeleteModal();
       }
     });
+
+    // Add event button add
+    this.btnAdd.addEventListener('click', () => {
+      this.showAdsModal(null);
+    });
   }
 
   /**
@@ -91,19 +104,76 @@ export class AdsView {
   }
 
   /**
-   * Shows the Add Ads modal.
+   * Displays the Ads modal with the provided adsData.
+   * If adsData is provided, it sets the modal title to 'Edit', otherwise 'Add'.
+   * Sets up event listeners for the modal buttons and form inputs.
+   * @param {Object} adsData - The data of the ad to be displayed in the modal.
    */
-  showAddAdsModal() {
-    const modalContent = generateModalAds(null, TITLE_MODAL.ADD);
+  showAdsModal(adsData) {
+    // Determine the title based on the presence of adsData
+    const title = adsData ? TITLE_MODAL.EDIT : TITLE_MODAL.ADD;
+
+    // Generate the modal content using adsData and the determined title
+    const modalContent = generateModalAds(adsData, title);
+
+    // Set the modal's HTML content and display it
     this.modalAds.innerHTML = modalContent;
     this.modalAds.style.display = DISPLAY_CLASS.FLEX;
 
-    const closeBtn = this.modalAds.querySelector('#close-modal-ads');
-    const cancelBtn = this.modalAds.querySelector('#add-ads-cancel');
+    // Get references to the close button, cancel button, submit button, and the ads form
+    const closeBtn = this.modalAds.querySelector(ELEMENT_ID.CLOSE_MODAL_ADS);
+    const cancelBtn = this.modalAds.querySelector(ELEMENT_ID.BTN_CANCEL);
+    const submitBtn = this.modalAds.querySelector(ELEMENT_ID.BTN_SUBMIT);
+    const formAds = this.modalAds.querySelector(ELEMENT_ID.FORM_ADS);
 
-    // Close modal button click and Cancel button click
+    // Add event listeners for close button and cancel button clicks
     closeBtn.addEventListener('click', this.closeModalHandler.bind(this));
     cancelBtn.addEventListener('click', this.closeModalHandler.bind(this));
+
+    // Handle the event of formatting phone number input
+    const phoneInput = formAds.querySelector(PROFILE_ADS.PHONE);
+    phoneInput.addEventListener('input', formatLimitedPhoneNumberInput);
+
+    // Handle the event of submitting the form
+    submitBtn.addEventListener('click', async () => {
+      // Extract values from the form inputs
+      const network = StrimmingString(formAds.querySelector(PROFILE_ADS.NETWORK).value);
+      const link = StrimmingString(formAds.querySelector(PROFILE_ADS.LINK).value);
+      const email = StrimmingString(formAds.querySelector(PROFILE_ADS.EMAIL).value);
+      const phone = StrimmingString(phoneInput.value);
+      const status = formAds.querySelector(PROFILE_ADS.STATUS_TYPE).value;
+
+      // Create an adsItem object with the extracted values
+      const adsItem = {
+        network,
+        link,
+        email,
+        phone,
+        status,
+      };
+
+      // Clear previous errors
+      this.clearErrorMessageForm();
+
+      // Validate the adsItem and show errors if any
+      const errors = validateAdsForm(adsItem);
+      if (Object.entries(errors).length > 0) {
+        showFormErrors(errors);
+      } else {
+        // If no errors, invoke the addAdsHandler and close the modal
+        await this.addAdsHandler(adsItem);
+
+        this.closeModalHandler();
+      }
+    });
+  }
+
+  /**
+   * Binds the handler for adding new ads.
+   * @param {Function} handler - The handler function for adding ads.
+   */
+  bindAddAds(handler) {
+    this.addAdsHandler = handler;
   }
 
   /**
@@ -183,7 +253,7 @@ export class AdsView {
    * Binds the delete user handler to the confirmation modal buttons.
    * @param {number} adsId - The ID of the ad to be deleted.
    */
-  bindDeleteUserHandler(adsId) {
+  bindDeleteAdsHandler(adsId) {
     this.confirmDeleteButton.setAttribute('data-id', adsId);
     this.showDeleteModal();
   }
@@ -192,7 +262,7 @@ export class AdsView {
    * Binds the delete user handler to the table element.
    * @param {Function} handler - The handler function for deleting an ad.
    */
-  bindDeleteUser(handler) {
+  bindDeleteAds(handler) {
     this.deleteHandler = handler;
   }
 
@@ -208,5 +278,17 @@ export class AdsView {
    */
   hideDeleteModal() {
     this.deleteModal.style.display = DISPLAY_CLASS.HIDDEN;
+  }
+
+  /**
+   * Clear Error Message for Form
+   */
+  clearErrorMessageForm() {
+    const errorFields = ['network', 'link', 'email', 'phone', 'status'];
+
+    errorFields.forEach(field => {
+      const errorElement = this.modalAds.querySelector(`#${field}-error`);
+      errorElement.textContent = '';
+    });
   }
 };
