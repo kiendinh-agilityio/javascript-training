9977c4f41a4ad96927740e17d353d29f867aef08
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-13 18:47:34 +0700

    feat: update constants message

--- src/scripts/constants/message.js
+++ src/scripts/constants/message.js
@@ -31,4 +31,7 @@ export const MESSAGE = {
   DELETE_ERROR: 'Failed to delete ads with Id',
   ADD_ERROR: 'Failed to add new ads',
   ADD_SUCCESS: 'You have added it successfully!',
+  EDIT_ERROR: 'Failed to edit ads',
+  EDIT_SUCCESS: 'You have edited it successfully!',
+  GET_DETAIL_ID_ERROR: 'Failed to get detail id ads',
 };
