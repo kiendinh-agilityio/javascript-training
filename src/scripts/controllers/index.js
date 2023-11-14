ac27f745be2843cf33eedad0fe63ff86fdf49cb6
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-14 23:28:07 +0700

    feat: update controller for the feature sort

--- src/scripts/controllers/index.js
+++ src/scripts/controllers/index.js
@@ -47,8 +47,11 @@ export class AdsController {
     // Bind edit handler to the view
     this.view.bindEditAds(this.handleEditAds.bind(this));
 
-    // add event edit
+    // Add event edit
     this.view.bindGetDetailAds(this.handleGetDetailAds.bind(this));
+
+    // Event Sort
+    this.view.setSortHandler(this.handleSort.bind(this));
   }
 
   /**
@@ -217,5 +220,47 @@ export class AdsController {
 
     // Display the advertisement modal with the retrieved details from the model.
     this.view.showAdsModal(this.model.adsDetail);
+  }
+
+  /**
+   * Sorts the adsData based on the specified column and order.
+   * @param {number} columnIndex - The index of the column to sort.
+   * @param {string} sortOrder - The sort order ('asc' or 'desc').
+   */
+  sortAdsData(columnIndex, sortOrder) {
+    const columnName = this.getColumnKey(columnIndex);
+
+    this.model.adsData = this.model.adsData.sort((a, b) => {
+      const valueA = a[columnName].toLowerCase();
+      const valueB = b[columnName].toLowerCase();
+
+      return (sortOrder === 'asc') ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);
+    });
+    this.view.displayAdsList(this.model.adsData);
+  }
+
+  /**
+   * Gets the corresponding key for the given column index.
+   * @param {number} columnIndex - The index of the column.
+   * @returns {string} - The key of the column.
+   */
+  getColumnKey(columnIndex) {
+    switch (columnIndex) {
+      case 0:
+        return 'network';
+      case 1:
+        return 'status';
+      default:
+        return '';
+    }
+  }
+
+  /**
+   * Handles the sorting of columns.
+   * @param {number} columnIndex - The index of the column to sort.
+   * @param {string} sortOrder - The sort order ('asc' or 'desc').
+   */
+  handleSort(columnIndex, sortOrder) {
+    this.sortAdsData(columnIndex, sortOrder);
   }
 }
