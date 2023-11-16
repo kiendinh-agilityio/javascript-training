9cf32cea9f0e25a842900e9996450a2d00348241
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-16 15:21:27 +0700

    feat: update generateAdsList

--- src/scripts/templates/generateAdsList.js
+++ src/scripts/templates/generateAdsList.js
@@ -1,22 +1,16 @@
 import { adsItem } from './adsItem';
 
-export const generateListAds = (items) => {
-  return `
-    <div class="table-container">
-      <ul class="flex flex-wrap justify-between table-row thead">
-        <li data-column="network" data-index="0">Network</li>
-        <li data-column="status" data-index="1">Status</li>
-        <li>Email</li>
-        <li>Phone</li>
-        <li></li>
-      </ul>
-      <div class="flex-column flex-wrap tbody relative">
-        ${items
-          .map((item) => {
-            return adsItem(item);
-          })
-          .join('')}
-      </div>
+export const generateListAds = (items) => `
+  <div class="table-container">
+    <ul class="flex flex-wrap justify-between table-row thead">
+      <li data-column="network" data-index="0">Network</li>
+      <li data-column="status" data-index="1">Status</li>
+      <li>Email</li>
+      <li>Phone</li>
+      <li></li>
+    </ul>
+    <div class="flex-column flex-wrap tbody relative">
+      ${items.map(adsItem).join('')}
     </div>
-  `;
-};
+  </div>
+`;
