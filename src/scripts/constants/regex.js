a069fe614204df538f48f44b2beaeb372a503867
Kien Dinh <kien.dinh@asnet.com.vn>
2023-11-16 15:18:53 +0700

    feat: update constants regex

--- src/scripts/constants/regex.js
+++ src/scripts/constants/regex.js
@@ -1,14 +1,19 @@
-// Check email have @ symbol, a string precedes it and the following string needs to contain a period, followed by 2-3 characters.
-export const REGEX_EMAIL = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+export const REGEX = {
+  // Check email have @ symbol, a string precedes it and the following string needs to contain a period, followed by 2-3 characters.
+  EMAIL: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
 
-// Check password
-export const REGEX_CHARACTERS = /^.{8,}$/;
+  // Check password
+  CHARACTERS: /^.{8,}$/,
 
-// Check regex for link
-export const REGEX_LINK = /^www\.[\w-]+\.com$/i;
+  // Check regex for link
+  LINK: /^www\.[\w-]+\.com$/i,
 
-// Check the character must match the phone number format. Example: (205)-205-5555
-export const REGEX_PHONE = /^\(\d{3}\)-\d{3}-\d{4}$/;
+  // Check the character must match the phone number format. Example: (205)-205-5555
+  PHONE: /^\(\d{3}\)-\d{3}-\d{4}$/,
 
-// Check characters with strings or numbers between 6 and 30 characters in length
-export const REGEX_NETWORK = /[0-9a-zA-Z]{4,20}/;
+  // Check characters with strings or numbers between 6 and 30 characters in length
+  NETWORK: /[0-9a-zA-Z]{4,20}/,
+
+  // Keyword
+  KEYWORD: /\s/g,
+};
